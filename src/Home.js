cadbc37fe85c97b9a90e4768d8b1d75c62d0fdc8
saliniyan <saliniyanp02@gmail.com>
2024-09-13 22:15:45 +0530

    used api

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,24 @@
 import React from "react";
 import './Home.css'
+import axios from "axios";
+
 function Home(){
+    const a=async()=>
+    {
+        try{
+            const books = await axios.get(`https://openlibrary.org/search.json?q=${'harry_potter'}&limit=4`)
+            console.log('fetched')
+        }
+        catch{
+            console.error("Error in fetching details")
+        }
+    }
     return(
         <div>
         <h1>Book Libreary App</h1>
-        
+        <div class='book'>
+            
+        </div>
         </div>
     )
 }
