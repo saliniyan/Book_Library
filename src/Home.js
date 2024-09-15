684e34a38cbb3855d1e454176a681326f0ba8ff9
saliniyan <saliniyanp02@gmail.com>
2024-09-15 18:24:02 +0530

    added searchbox

--- src/Home.js
+++ src/Home.js
@@ -4,38 +4,60 @@ import axios from "axios";
 import { useState } from "react";
 
 function Home(){
-    const [books,setbooks]=useState([])
-    const [offset,setoffset]=useState(0)
+    const [books,setBooks]=useState([])
+    const [offset,setOffset]=useState(0)
+    const [search,setSearch]=useState('Marvel')
 
-    const a=async()=>
+    const fetchbook=async(e)=>
     {
         try{
-            const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=william+shakesphere&maxResults=4&startIndex=${offset}`);            
-            setbooks(prevbook=>[...prevbook,...response.data.items])
-            setoffset(offset+4)
+            e.preventDefault();
+            const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${search}&maxResults=4&startIndex=${offset}`);            
+            setBooks(prevbook=>[...prevbook,...response.data.items])
+            setOffset(offset+4)
             console.log('fetched')
         }
         catch{
             console.error("Error in fetching details")
         }
     }
+
+    const handlesearch=(e)=>
+    {
+        setSearch(e.target.value);
+        setOffset(0);
+    }
+
     return(
         <div>
-        <h1>Book Libreary App</h1>
-        <button onClick={a}>fetch Book</button>
+        <h1>Book Library App</h1>
+        <form onSubmit={fetchbook}>
+        <div>
+            <input className="searchbox"
+                type="text"
+                onChange={handlesearch}
+                placeholder="Search here..."
+            />
+            <button type="submit" className="searchbutton">Search</button>
+        </div>
+        </form>
         <div className='book'>
             {books.map((book,index)=>(
                 <div key={index} className="booklist">
-                    <img src={book.volumeInfo.imageLinks.thumbnail} alt={`${book.volumeInfo.title} cover`} />
+                    <img src={book.volumeInfo.imageLinks?.thumbnail} alt={`${book.volumeInfo.title} cover`} />
                     <h3>{book.volumeInfo.title}</h3>
                     <p>{book.volumeInfo.authors}</p>
                     <p>Publisher: {book.volumeInfo.publisher}</p>
                     <p>Published Date: {book.volumeInfo.publishedDate}</p>
+                    <p>categories: {book.volumeInfo.categories}</p>
+                    <p>Book description: {book.volumeInfo.description?book.volumeInfo.description.slice(0,100):"description unavailable"}...</p>
                     <a href={book.volumeInfo.previewLink}>click here</a>
+                    
                 </div>
             ))
             }
         </div>
+        <button onClick={fetchbook} className="button">Load Books</button>
         </div>
     )
 }
