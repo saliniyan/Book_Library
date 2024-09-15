af23e8d8d885e220d19ffe6920f24125dd1c2747
saliniyan <saliniyanp02@gmail.com>
2024-09-15 09:23:46 +0530

    included displaying books with offset

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,18 @@
 import React from "react";
 import './Home.css'
 import axios from "axios";
+import { useState } from "react";
 
 function Home(){
+    const [books,setbooks]=useState([])
+    const [offset,setoffset]=useState(0)
+
     const a=async()=>
     {
         try{
-            const books = await axios.get(`https://openlibrary.org/search.json?q=${'harry_potter'}&limit=4`)
+            const response = await axios.get(`https://openlibrary.org/search.json?q=${'harry_potter'}&limit=4&offset=${offset}`)
+            setbooks(prevbook=>[...prevbook,...response.data.docs])
+            setoffset(offset+4)
             console.log('fetched')
         }
         catch{
@@ -16,8 +22,15 @@ function Home(){
     return(
         <div>
         <h1>Book Libreary App</h1>
-        <div class='book'>
-            
+        <button onClick={a}>fetch Book</button>
+        <div className='book'>
+            {books.map((book,index)=>(
+                <div key={index}>
+                    <h2>{book.title}</h2>
+                    <p>{book.author_name}</p>
+                </div>
+            ))
+            }
         </div>
         </div>
     )
