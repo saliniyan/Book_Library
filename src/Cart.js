6f7eb33a3cadc5f746bc6f689f83f7ff56ac3611
saliniyan <saliniyanp02@gmail.com>
2024-09-17 09:42:08 +0530

    inlcuded inital book and local storage

--- src/Cart.js
+++ src/Cart.js
@@ -1,11 +1,18 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import { useLocation } from 'react-router-dom';
 import './Home.css'
 import './Header.css'
 
 function Cart() {
     const location = useLocation();
-    const [items,setItems] = useState(location.state?.items || []);
+    const [items, setItems] = useState(() => {
+        const storedItems = localStorage.getItem('items');
+        return storedItems ? JSON.parse(storedItems) : location.state?.items || [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem('items', JSON.stringify(items));
+    }, [items]);
 
     const remove=(a)=>{
         setItems(items.filter((item) => item.title!==a))
@@ -17,14 +24,14 @@ function Cart() {
         <div className='book'>
             {items.map((item,index)=>(
                 <div key={index} className="booklist">
-                    <h3>{item.title}</h3>
-                    <img src={item.image} alt={'cover'}  className='img1'/>
-                    <p>Authors: {item.authors.join(", ")}</p>
-                    <p>Publisher: {item.publisher}</p>
-                    <p>Published Date: {item.publishedDate}</p>
-                    <p>Categories: {item.categories.join(", ")}</p>
-                    <p>Description: {item.description}</p>
-                    <a href={item.previewLink}>Preview Link</a>
+                    <h3>{item.title || 'Title Unknown'}</h3>
+                    <img src={item.image || 'default-image-url.jpg'} alt={`${item.title || 'cover'}`} className='img1'/>
+                    <p>Authors: {item.authors?.join(", ") || 'Unknown Author(s)'}</p>
+                    <p>Publisher: {item.publisher || 'Unknown Publisher'}</p>
+                    <p>Published Date: {item.publishedDate || 'Unknown Date'}</p>
+                    <p>Categories: {item.categories?.join(", ") || 'Unknown Categories'}</p>
+                    <p>Description: {item.description || 'No description available'}</p>
+                    <a href={item.previewLink || '#'}>Preview Link</a>
                     <br />
                     <button onClick={()=>remove(item.title)} className='button2'>Remove book</button>
                 </div>
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,74 @@
 import React from "react";
 import './Home.css'
 import axios from "axios";
-import { useState } from "react";
+import { useState} from "react";
 import {useNavigate } from "react-router-dom"
 
 function Home(){
-    const [books,setBooks]=useState([])
-    const [offset,setOffset]=useState(0)
+    const [books, setBooks] = useState([
+        {
+            id: '1',
+            volumeInfo: {
+                title: 'The Reign of Marvel Studios',
+                authors: ['Joanna Robinson', 'Dave Gonzales', 'Gavin Edwards'],
+                publisher: 'Liveright Publishing',
+                publishedDate: '2023-10-10',
+                categories: ['Performing Arts'],
+                description: 'INSTANT NEW YORK TIMES BESTSELLER "A superb chronicle of how Marvel Studios conquered Hollywood"',
+                imageLinks: {
+                    thumbnail: 'http://books.google.com/books/content?id=CYqpEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+                },
+                previewLink: 'https://books.google.co.in/books?id=CYqpEAAAQBAJ&printsec=frontcover&dq=Marvel&hl=&cd=1&source=gbs_api'
+            }
+        },
+        {
+            id: '2',
+            volumeInfo: {
+                title: 'Marvel Encyclopedia New Edition',
+                authors: ['Stephen Wiacek', 'DK', 'Stan Lee', 'Adam Bray'],
+                publisher: 'Dorling Kindersley Ltd',
+                publishedDate: '2018-09-13',
+                categories: ['Comics & Graphic Novels'],
+                description: 'Join Captain America, Thor, Iron Man, Black Widow, and the Guardians of the Galaxy on a stunning journey.',
+                imageLinks: {
+                    thumbnail: 'http://books.google.com/books/content?id=NgXeDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+                },
+                previewLink: 'https://books.google.co.in/books?id=NgXeDwAAQBAJ&printsec=frontcover&dq=Marvel&hl=&cd=2&source=gbs_api#v=onepage&q=Marvel&f=false'
+            }
+        },
+        {
+            id: '3',
+        volumeInfo: {
+            title: 'Marvel Studios Visual Dictionary',
+            authors: ['Adam Bray'],
+            publisher: 'Dorling Kindersley Ltd',
+            publishedDate: '2018-09-13',
+            categories: ['Comics & Graphic Novels'],
+            description: 'Join Captain America, Thor, Iron Man, Black Widow, and the Guardians of the Galaxy on a stunning journey.',
+            imageLinks: {
+                thumbnail: 'http://books.google.com/books/content?id=XbF2EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api' 
+            },
+            previewLink: 'http://books.google.co.in/books?id=XbF2EAAAQBAJ&printsec=frontcover&dq=Marvel&hl=&cd=3&source=gbs_api'
+        }
+    },
+    {
+        id: '4',
+        volumeInfo: {
+            title: 'Make Ours Marvel',
+            authors: ['Matt Yockey'],
+            publisher: 'University of Texas Press',
+            publishedDate: '2017-06-20',
+            categories: ['Business & Economics'],
+            description: 'Tracing the rise of the Marvel Comics brand from the creation of the Fantastic Four to the development of a global entertainment phenomenon.',
+            imageLinks: {
+                thumbnail: 'http://books.google.com/books/content?id=FMigDgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+            },
+            previewLink: 'hhttp://books.google.co.in/books?id=FMigDgAAQBAJ&printsec=frontcover&dq=Marvel&hl=&cd=4&source=gbs_api'
+        }
+    }
+    ]);
+    
+    const [offset,setOffset]=useState(4)
     const [search,setSearch]=useState('Marvel')
     const [item,setItem]=useState([])
     const [message, setMessage] = useState('');
@@ -77,10 +139,10 @@ function Home(){
                 <div key={index} className="booklist">
                     <img src={book.volumeInfo.imageLinks?.thumbnail} alt={`${book.volumeInfo.title} cover`} className="img1"/>
                     <h3>{book.volumeInfo.title?book.volumeInfo.title:'Title Unknown'}</h3>
-                    <p>{book.volumeInfo.authors?book.volumeInfo.authors:'Authon Unknown'}</p>
+                    <p>{book.volumeInfo.authors?book.volumeInfo.authors:'Author Unknown'}</p>
                     <p>Publisher: {book.volumeInfo.publisher?book.volumeInfo.publisher:'Publisher Unknown'}</p>
-                    <p>Published Date: {book.volumeInfo.publisher?book.volumeInfo.publisher:'Date Unknown'}</p>
-                    <p>categories: {book.volumeInfo.publisher?book.volumeInfo.publisher:'categories Unknown'}</p>
+                    <p>Published Date: {book.volumeInfo.publishedDate?book.volumeInfo.publishedDate:'Date Unknown'}</p>
+                    <p>categories: {book.volumeInfo.categories?book.volumeInfo.categories:'categories Unknown'}</p>
                     <p>Book description: {book.volumeInfo.description?book.volumeInfo.description.slice(0,100):"description unavailable"}...</p>
                     <a href={book.volumeInfo.previewLink}>click here</a>
                     <br />
