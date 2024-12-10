7a8d11fababa3b97932fb1aeb03e1e73e33e0931
saliniyan <saliniyanp02@gmail.com>
2024-12-10 07:18:19 +0530

    modified about page

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
     <div className="home-container">
       <Navbar />
       <h1>About Us</h1>
-      <p>House Finder is dedicated to simplifying the house hunting process. We understand that moving to a new city can be overwhelming, and our mission is to make your search as seamless as possible.</p>
+      <p>Book library is dedicated to simplifying the Book hunting process. We understand that seaching a book can be overwhelming, and our mission is to make your search as seamless as possible.</p>
       <h2>Our Vision</h2>
       <p>To be the most trusted platform for finding rental homes, offering the best user experience and comprehensive property listings.</p>
       <h2>Our Team</h2>
