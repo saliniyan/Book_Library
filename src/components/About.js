a912966112e6fc56578756d771da2e22288705b4
saliniyan <saliniyanp02@gmail.com>
2024-12-10 07:19:05 +0530

    modified about page

--- src/components/About.js
+++ src/components/About.js
@@ -9,9 +9,9 @@ const About = () => {
       <h1>About Us</h1>
       <p>Book library is dedicated to simplifying the Book hunting process. We understand that seaching a book can be overwhelming, and our mission is to make your search as seamless as possible.</p>
       <h2>Our Vision</h2>
-      <p>To be the most trusted platform for finding rental homes, offering the best user experience and comprehensive property listings.</p>
+      <p>To be the most trusted platform for finding Books, offering the best user experience and comprehensive property listings.</p>
       <h2>Our Team</h2>
-      <p>Our team consists of real estate experts, web developers, and customer service professionals committed to helping you find your next home.</p>
+      <p>...</p>
     </div>
   );
 };
